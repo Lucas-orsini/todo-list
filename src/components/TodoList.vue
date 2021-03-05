4a18f9833984fdf8e58e72dcc36de8c354a5d6f2
Lucas o <lucas.orsini@outlook.fr>
2021-03-05 09:06:22 +0100

    yo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,26 +1,22 @@
 <template>
     
 
-<div class="newTodo" v-for="items in tasktab" :key="items"> 
+<div class="newTodo" v-for="items in tasktab" :key="items" :items="items"> 
     <input type="checkbox" name="done" id="checkbox" v-model="checked" :key="items">
     <p :key="items">{{items.taskName}}</p>
-    <input type="button" name="delete" id="delete">
+    <input type="button" name="delete" id="delete" :key="items">
     </div>
 </template>
-<!--
+
 <script>
 export default {
     props : {
-        tasktab : {
+        tasktab: {
             type : Array
         }
     },
-    data(){
-        return {
-
-        }
     }
-}
+
 </script>
 
 <style scoped>
